1e90d6fc7b192d91ed580296ad5073346da2860f
Вайбкодер1337 <301549628+HVHBIGNAME@users.noreply.github.com>
2026-09-03 14:20:37 +0300

    worldgen: density probe example (diagnostic)

--- crates/bcore-worldgen/examples/vanilla_probe.rs
+++ crates/bcore-worldgen/examples/vanilla_probe.rs
@@ -1,64 +1,14 @@
-//! Probe final_density's two min-arguments separately (after fixes).
+//! Isolate interpolated: should return the inner constant.
 use bcore_worldgen::density::{self, EvalContext};
 
 fn main() {
-    let root = "target/datapack";
-    let settings = std::fs::read_to_string(format!(
-        "{root}/data/minecraft/worldgen/noise_settings/overworld.json"
-    ))
-    .unwrap();
-    let final_json = settings
-        .split("\"final_density\":")
-        .nth(1)
-        .unwrap()
-        .split("\"vein_toggle\"")
-        .next()
-        .unwrap();
-    let arg1 = sub_obj(final_json, "\"argument1\":");
-    let arg2 = sub_obj(final_json, "\"argument2\":");
-    let f1 = density::parse_json(&arg1).unwrap();
-    let f2 = density::parse_json(&arg2).unwrap();
     let ctx = EvalContext::default();
-    for y in [-64.0, 0.0, 63.0, 64.0, 120.0] {
-        let a = density::evaluate(&f1, 8.0, y, 8.0, &ctx);
-        let b = density::evaluate(&f2, 8.0, y, 8.0, &ctx);
-        println!(
-            "y={y:>5}: squeeze={a:>8.4}  noodle={b:>8.4}  min={:.4}",
-            a.min(b)
-        );
+    // interpolated(constant 5) should be 5 everywhere.
+    let f = density::parse_json(
+        r#"{"type":"minecraft:interpolated","argument":{"type":"minecraft:constant","value":5}}"#,
+    )
+    .unwrap();
+    for (x, y, z) in [(8.0, 64.0, 8.0), (9.0, 65.0, 9.0), (0.0, 0.0, 0.0)] {
+        println!("interpolated(5) at ({x},{y},{z}) = {:.4}", density::evaluate(&f, x, y, z, &ctx));
     }
-}
-
-fn sub_obj(s: &str, key: &str) -> String {
-    let p = s.find(key).unwrap() + key.len();
-    let rest = &s[p..];
-    let start = rest.find('{').or_else(|| rest.find('"')).unwrap();
-    let b = rest.as_bytes();
-    let mut depth = 0i32;
-    let mut in_str = false;
-    let mut i = start;
-    let mut end = rest.len();
-    while i < b.len() {
-        let c = b[i] as char;
-        if in_str {
-            if c == '"' {
-                in_str = false;
-            }
-        } else {
-            match c {
-                '"' => in_str = true,
-                '{' => depth += 1,
-                '}' => {
-                    depth -= 1;
-                    if depth == 0 {
-                        end = i + 1;
-                        break;
-                    }
-                }
-                _ => {}
-            }
-        }
-        i += 1;
-    }
-    rest[start..end].to_string()
 }
